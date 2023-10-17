8c235818dc13b15e6c40f4994d25bafec7de6144
Warren Gonzalez <warren0419@outlook.com>
2023-10-16 18:42:01 -0600

    portafolio v1.0

--- public/controllers/portfolioController.js
+++ public/controllers/portfolioController.js
@@ -1,2 +1,35 @@
 'use strict';
 
+document.addEventListener("DOMContentLoaded", function () {
+    function textTyped() {
+        var typed = new Typed(".auto-type", {
+            strings: ["front end developer", "backend developer", "lead", "web designer", "UX/UI developer", "web tester", "freelancer"],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true
+        });
+    }
+
+    function analytics() {
+
+
+        try {
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+
+            gtag('config', 'G-YJC0LM555D');
+
+            console.log('Obteniendo Google Analytics' + "\n" +  Date());
+        } catch (error) {
+            console.error("Erroe al compilar Google Analyrics " + error.message);
+        }
+    }
+
+    analytics();
+    textTyped();
+});
+
+
+
+
